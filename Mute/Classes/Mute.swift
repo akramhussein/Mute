79c32e9a2a1ac92eaad53a8670f87a3c6ce2d9e3
Akram Hussein <akram@project-sapiens.com>
2018-04-14 19:46:25 +0100

    Lint cleanup

--- Mute/Classes/Mute.swift
+++ Mute/Classes/Mute.swift
@@ -12,32 +12,32 @@ import AudioToolbox
 public class Mute: NSObject {
 
     public typealias MuteNotificationCompletion = ((_ mute: Bool) -> Void)
-    
+
     // MARK: Properties
-    
+
     /// Shared instance
     public static let shared = Mute()
 
     /// Sound ID for mute sound
     private let soundUrl = Mute.muteSoundUrl
-    
+
     /// Should notify every second or only when changes?
     /// True will notify every second of the state, false only when it changes
     public var alwaysNotify = true
-    
+
     /// Notification handler to be triggered when mute status changes
     /// Triggered every second if alwaysNotify=true, otherwise only when it switches state
     public var notify: MuteNotificationCompletion?
-    
+
     /// Currently playing? used when returning from the background (if went to background and foreground really quickly)
     public private(set) var isPlaying = false
-    
+
     /// Current mute state
     public private(set) var isMute = false
-    
+
     /// Sound is scheduled
     private var isScheduled = false
-    
+
     /// State of detection - paused when in background
     public var isPaused = false {
         didSet {
@@ -56,25 +56,25 @@ public class Mute: NSObject {
             }
         }
     }
-    
+
     /// Silent sound (0.5 sec)
     private var soundId: SystemSoundID = 0
-    
+
     /// Time difference between start and finish of mute sound
     private var interval: TimeInterval = 0
-    
+
     // MARK: Resources
-    
+
     /// Library bundle
     private static var bundle: Bundle {
         guard let path = Bundle(for: Mute.self).path(forResource: "Mute", ofType: "bundle"),
             let bundle = Bundle(path: path) else {
-                fatalError("Mute.bundle not found")
+            fatalError("Mute.bundle not found")
         }
-        
+
         return bundle
     }
-    
+
     /// Mute sound url path
     private static var muteSoundUrl: URL {
         guard let muteSoundUrl = Mute.bundle.url(forResource: "mute", withExtension: "aiff") else {
@@ -82,51 +82,51 @@ public class Mute: NSObject {
         }
         return muteSoundUrl
     }
-    
+
     // MARK: Init
-    
+
     /// private init
     private override init() {
         super.init()
-        
+
         self.soundId = 1
-        
+
         if AudioServicesCreateSystemSoundID(self.soundUrl as CFURL, &self.soundId) == kAudioServicesNoError {
             let weakSelf = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
-            
-            AudioServicesAddSystemSoundCompletion(self.soundId, CFRunLoopGetMain(), CFRunLoopMode.defaultMode.rawValue, { soundId, weakSelfPointer in
+
+            AudioServicesAddSystemSoundCompletion(self.soundId, CFRunLoopGetMain(), CFRunLoopMode.defaultMode.rawValue, { _, weakSelfPointer in
                 guard let weakSelfPointer = weakSelfPointer else { return }
-                                                                
+
                 let weakSelfValue = Unmanaged<Mute>.fromOpaque(weakSelfPointer).takeUnretainedValue()
                 weakSelfValue.soundFinishedPlaying()
-                
+
             }, weakSelf)
-            
+
             var yes: UInt32 = 1
             AudioServicesSetProperty(kAudioServicesPropertyIsUISound,
                                      UInt32(MemoryLayout.size(ofValue: self.soundId)),
                                      &self.soundId,
                                      UInt32(MemoryLayout.size(ofValue: yes)),
                                      &yes)
-            
+
             self.schedulePlaySound()
         } else {
             print("Failed to setup sound player")
             self.soundId = 0
         }
-        
+
         // Notifications
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(Mute.didEnterBackground(_:)),
                                                name: NSNotification.Name.UIApplicationDidEnterBackground,
                                                object: nil)
-        
+
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(Mute.willEnterForeground(_:)),
                                                name: NSNotification.Name.UIApplicationWillEnterForeground,
                                                object: nil)
     }
-    
+
     deinit {
         if self.soundId != 0 {
             AudioServicesRemoveSystemSoundCompletion(self.soundId)
@@ -134,42 +134,40 @@ public class Mute: NSObject {
         }
         NotificationCenter.default.removeObserver(self)
     }
-    
+
     // MARK: Notification Handlers
-    
+
     /// Selector called when app enters background
     @objc private func didEnterBackground(_ sender: Any) {
         self.isPaused = true
     }
-    
+
     /// Selector called when app will enter foreground
     @objc private func willEnterForeground(_ sender: Any) {
         self.isPaused = false
     }
-    
+
     // MARK: Methods
-    
+
     /// Schedueles mute sound to be played in 1 second
     private func schedulePlaySound() {
         /// Don't schedule a new one if we already have one queued
-        if (self.isScheduled) {
-            return;
-        }
-        
+        if self.isScheduled { return }
+
         self.isScheduled = true
-        
+
         DispatchQueue.main.asyncAfter(deadline: .now() + self.checkInterval) {
             self.isScheduled = false
-            
+
             /// Don't play if we're paused
-            if (self.isPaused) {
-                return;
+            if self.isPaused {
+                return
             }
-            
+
             self.playSound()
         }
     }
-    
+
     /// If not paused, playes mute sound
     private func playSound() {
         if !self.isPaused {
@@ -178,14 +176,14 @@ public class Mute: NSObject {
             AudioServicesPlaySystemSound(self.soundId)
         }
     }
-    
+
     /// Called when AudioService finished playing sound
     private func soundFinishedPlaying() {
         self.isPlaying = false
-        
+
         let elapsed = Date.timeIntervalSinceReferenceDate - self.interval
         let isMute = elapsed < 0.1
-        
+
         if self.isMute != isMute || self.alwaysNotify {
             self.isMute = isMute
             self.notify?(isMute)
